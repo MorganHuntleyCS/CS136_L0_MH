b6c228d554ace199c9213d80a1c71be7b5d9fe08
MorganHuntleyCS <huntlemt1717@student.laccd.edu>
2025-09-07 14:32:54 -0700

    Add files via upload

---
+++ CS136_L0_MH.cpp
@@ -0,0 +1,33 @@
+// MORGAN HUNTLEY
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "\nMy name is Morgan Huntley " << endl;
+
+    cout << "\nEnter the first number >";
+    cin >> num1;
+
+    cout << "\nEnter the second number  ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "\nThe sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
+
+/*
+  My name is Morgan Huntley
+
+Enter the first number >1
+
+Enter the second number  2
+
+The sum of 1 and 2 is 3
+*/
